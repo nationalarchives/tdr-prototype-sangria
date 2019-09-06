6b610eef2531684ed5aa96c4dd8b7c178d3fc469
TomJKing <Tom.King@nationalarchives.gov.uk>
2019-09-06 07:25:17 +0100

    DB migration fixed so dummy timestamps with timezone are added

--- migrations/sql/V10__Change_file_last_modified_column_date_type.sql
+++ migrations/sql/V10__Change_file_last_modified_column_date_type.sql
@@ -2,6 +2,7 @@
   Switch the file table from using string last_modified_dates to timestamp with timezone to store dates correctly.
  */
 
-ALTER TABLE file
-  ALTER COLUMN last_modified_date
-  TYPE TIMESTAMPTZ USING last_modified_date::TIMESTAMPTZ;
+ALTER TABLE file ADD COLUMN placeholder TIMESTAMPTZ DEFAULT NOW();
+ALTER TABLE file ALTER COLUMN placeholder SET NOT NULL;
+ALTER TABLE file DROP COLUMN last_modified_date;
+ALTER TABLE file RENAME COLUMN placeholder TO last_modified_date;
