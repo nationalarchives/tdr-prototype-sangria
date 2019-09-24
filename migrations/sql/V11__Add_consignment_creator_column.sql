15259a86387b92aa5966941b71fbc35bcd3705ce
Ian Hoyle <ian.hoyle@nationalarchives.gov.uk>
2019-09-24 09:24:51 +0100

    Use default values for consignment so can alter consignment if data already present

--- migrations/sql/V11__Add_consignment_creator_column.sql
+++ migrations/sql/V11__Add_consignment_creator_column.sql
@@ -2,6 +2,7 @@
   Add creator - use Cognito user name for Alpha.
  */
 
-ALTER TABLE consignments
-  ADD COLUMN creator VARCHAR (50) NOT NULL,
-  ADD COLUMN transferring_body VARCHAR (50) NOT NULL;
+ALTER TABLE consignments ADD COLUMN creator VARCHAR (50) DEFAULT 'creator';
+ALTER TABLE consignments ALTER COLUMN creator SET NOT NULL;
+ALTER TABLE consignments  ADD COLUMN transferring_body VARCHAR (50) DEFAULT 'transferring body';
+ALTER TABLE consignments ALTER COLUMN transferring_body SET NOT NULL;
