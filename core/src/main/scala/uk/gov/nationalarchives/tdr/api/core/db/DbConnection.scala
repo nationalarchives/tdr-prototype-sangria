14552c6b600f577ee82c59a92164b019fde0d2d7
TomJKing <Tom.King@nationalarchives.gov.uk>
2019-11-07 08:21:10 +0000

    Change name of config variables

--- core/src/main/scala/uk/gov/nationalarchives/tdr/api/core/db/DbConnection.scala
+++ core/src/main/scala/uk/gov/nationalarchives/tdr/api/core/db/DbConnection.scala
@@ -4,15 +4,15 @@ import com.typesafe.config.{ConfigFactory, ConfigValueFactory}
 import slick.jdbc.PostgresProfile.api._
 
 object DbConnection {
-  private val dbConfig = sys.env.get("TDR_API_ENVIRONMENT") match {
+  private val connectionParameters = sys.env.get("TDR_API_ENVIRONMENT") match {
     case Some("TEST") => PrototypeDbConfig
     case _ => DevDbConfig
   }
 
-  private val config = ConfigFactory.empty()
-    .withValue("db.url", ConfigValueFactory.fromAnyRef(dbConfig.url))
-    .withValue("db.user", ConfigValueFactory.fromAnyRef(dbConfig.username))
-    .withValue("db.password", ConfigValueFactory.fromAnyRef(dbConfig.password))
+  private val postgresConfig = ConfigFactory.empty()
+    .withValue("db.url", ConfigValueFactory.fromAnyRef(connectionParameters.url))
+    .withValue("db.user", ConfigValueFactory.fromAnyRef(connectionParameters.username))
+    .withValue("db.password", ConfigValueFactory.fromAnyRef(connectionParameters.password))
 
-  val db = Database.forConfig("db", config, new org.postgresql.Driver)
+  val db = Database.forConfig("db", postgresConfig, new org.postgresql.Driver)
 }
